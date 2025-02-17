aee6cbb65e91fcbf32559188a2f7cf9e74be23fe
bitDig6 <ummenabilajalal@gmail.com>
2025-02-17 20:47:20 +0600

    made the graph responsive and working on sort functionalities

--- src/components/ListedBooks/ListedBooks.jsx
+++ src/components/ListedBooks/ListedBooks.jsx
@@ -10,29 +10,57 @@ const ListedBooks = () => {
     const allBooks = useLoaderData();
     const [readList, setReadList] = useState([]);
     const [wishList, setWishList] = useState([]);
+    const [sort, setSort] = useState('');
+    // const [tabNo, setTabNo] = useState(1);
 
-    useEffect( () => {
+    useEffect(() => {
         const storedReadList = getStoredReadList();
-        const storedReadListInt = storedReadList.map( id => parseInt(id));
+        const storedReadListInt = storedReadList.map(id => parseInt(id));
         const storedWishList = getStoredWishList();
         const storedWishListInt = storedWishList.map(id => parseInt(id));
 
-        const readBookList = allBooks.filter( book => storedReadListInt.includes(book.bookId) );
-        const wishBookList = allBooks.filter( book => storedWishListInt.includes(book.bookId));
-
-
-        // console.log(storedReadList, storedReadListInt, readBookList);
-        // console.log(storedWishList, storedWishListInt, wishBookList);
+        const readBookList = allBooks.filter(book => storedReadListInt.includes(book.bookId));
+        const wishBookList = allBooks.filter(book => storedWishListInt.includes(book.bookId));
 
         setReadList(readBookList);
         setWishList(wishBookList);
-    }, [])
+    }, []);
 
-    // console.log(allBooks);
+    // const saveTabNumber = num => {
+    //     setTabNo(num);
+    //     console.log(`You are on Tab ${num}`);
+    // }
+
+    const handleSort = sortType => {
+        setSort(sortType);
+        if (sortType === 'No. of Pages') {
+            const sortedReadList = [...readList].sort((a, b) => a.totalPages - b.totalPages);
+            setReadList('sorted by pages', sortedReadList);
+        }
+        if (sortType === 'Ratings') {
+            const sortedReadList = [...readList].sort((a, b) => a.rating - b.rating);
+            setReadList(sortedReadList);
+            console.log('sorted by ratings', sortedReadList);
+        }
+
+    }
 
     return (
         <div>
-            <h3 className="text-3xl my-8">Listed Books</h3>
+            <div className='flex flex-col items-center'>
+                <h3 className="text-3xl font-bold my-6">Listed Books</h3>
+                <div className="dropdown">
+                    <div tabIndex={0} role="button" className="btn m-1 font-semibold text-lg bg-lime-500 text-white">
+                        {
+                            sort ? `Sort By ${sort}` : 'Sort By'
+                        }
+                    </div>
+                    <ul tabIndex={0} className="dropdown-content menu bg-base-100 rounded-box z-1 w-52 p-2 shadow-sm">
+                        <li onClick={() => handleSort('Ratings')}><a>Ratings</a></li>
+                        <li onClick={() => handleSort('No. of Pages')}><a>No. of Pages</a></li>
+                    </ul>
+                </div>
+            </div>
             <Tabs>
                 <TabList>
                     <Tab>Read List</Tab>
@@ -42,15 +70,14 @@ const ListedBooks = () => {
                 <TabPanel>
                     <div className='flex flex-col gap-5'>
                         {
-                            readList.map(book => <Book key={book.bookId} book={book}></Book> )
+                            readList.map(book => <Book key={book.bookId} book={book}></Book>)
                         }
                     </div>
                 </TabPanel>
                 <TabPanel>
-                    
-                <div className='flex flex-col gap-5'>
+                    <div className='flex flex-col gap-5'>
                         {
-                            wishList.map(book => <Book key={book.bookId} book={book}></Book> )
+                            wishList.map(book => <Book key={book.bookId} book={book}></Book>)
                         }
                     </div>
                 </TabPanel>
--- src/components/PagesToRead/PagesToRead.jsx
+++ src/components/PagesToRead/PagesToRead.jsx
@@ -1,58 +1,61 @@
-import { BarChart, Bar, Cell, XAxis, YAxis, CartesianGrid } from 'recharts';
+import { BarChart, Bar, Cell, XAxis, YAxis, CartesianGrid, ResponsiveContainer } from 'recharts';
 import PropTypes from 'prop-types';
 import { useLoaderData } from 'react-router';
 
 const PagesToRead = () => {
-    const colors = ['#0088FE', '#00C49F', '#FFBB28', '#FF8042', 'red', 'pink'];
+  const colors = ['#0088FE', '#00C49F', '#FFBB28', '#FF8042', 'red', 'pink'];
 
-    const data = useLoaderData();
+  const data = useLoaderData();
 
-    const getPath = (x, y, width, height) => {
-        return `M${x},${y + height}C${x + width / 3},${y + height} ${x + width / 2},${y + height / 3}
+  const getPath = (x, y, width, height) => {
+    return `M${x},${y + height}C${x + width / 3},${y + height} ${x + width / 2},${y + height / 3}
   ${x + width / 2}, ${y}
   C${x + width / 2},${y + height / 3} ${x + (2 * width) / 3},${y + height} ${x + width}, ${y + height}
   Z`;
-    };
+  };
 
-    const TriangleBar = (props) => {
-        const { fill, x, y, width, height } = props;
+  const TriangleBar = (props) => {
+    const { fill, x, y, width, height } = props;
 
-        return <path d={getPath(x, y, width, height)} stroke="none" fill={fill} />;
-    };
+    return <path d={getPath(x, y, width, height)} stroke="none" fill={fill} />;
+  };
 
 
-    return (
-        <div className='my-10'>
-            <BarChart
-      width={1000}
-      height={500}
-      data={data}
-      margin={{
-        top: 20,
-        right: 30,
-        left: 20,
-        bottom: 130,
-      }}
-    >
-      <CartesianGrid strokeDasharray="3 3" />
-      <XAxis dataKey="bookName" interval={0} angle={-30} textAnchor='end' dy={10}/>
-      <YAxis />
-      <Bar dataKey="totalPages" fill="#8884d8" shape={<TriangleBar />} label={{ position: 'top' }}>
-        {data.map((entry, index) => (
-          <Cell key={`cell-${index}`} fill={colors[index % 20]} />
-        ))}
-      </Bar>
-    </BarChart>
-        </div>
-    );
+  return (
+    <div style={ {width: '100%', height: '400px', margin: '0 auto'} }>
+      <ResponsiveContainer width="90%" height="100%">
+        <BarChart
+          width={1000}
+          height={500}
+          data={data}
+          margin={{
+            top: 20,
+            right: 30,
+            left: 20,
+            bottom: 20,
+          }}
+        >
+          <CartesianGrid strokeDasharray="3 3" />
+          {/* <XAxis dataKey="bookName" interval={0} angle={-30} textAnchor='end' dx={20} dy={10} /> */}
+          <XAxis dataKey="bookId" label={{ value: 'Book Ids', position: 'bottom'}}></XAxis>
+          <YAxis label={{ value: 'No. of Pages', angle: -90, position: 'insideLeft' }}/>
+          <Bar dataKey="totalPages" fill="#8884d8" shape={<TriangleBar />} label={{ position: 'top' }}>
+            {data.map((entry, index) => (
+              <Cell key={`cell-${index}`} fill={colors[index % 20]} />
+            ))}
+          </Bar>
+        </BarChart>
+      </ResponsiveContainer>
+    </div>
+  );
 };
 
 PagesToRead.propTypes = {
-    fill: PropTypes.string,
-    x: PropTypes.number,
-    y: PropTypes.number,
-    width: PropTypes.number,
-    height: PropTypes.number
+  fill: PropTypes.string,
+  x: PropTypes.number,
+  y: PropTypes.number,
+  width: PropTypes.number,
+  height: PropTypes.number
 }
 
 export default PagesToRead;
